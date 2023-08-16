94b11bf7429c7d390e6809360b27277d32aa1354
Karthik Udupa <karthik.udupa@bottomline.com>
2023-08-16 13:38:54 +0530

    main

---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CakeContainer = () => {
+  return (
+    <>
+      <h2>Number of Cakes</h2>
+      <button>Buy Cakes</button>
+    </>
+  );
+};
+
+export default CakeContainer;
